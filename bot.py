4246d867a8914898b183fb0ce0f3777ba24cc766
gghackboy06-bit <gghackboy06@gmail.com>
2026-09-02 14:03:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-bot.py
+import telebot
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(
+        message,
+        "🎮 Welcome to Carrom Pool Key Store!\n\n"
+        "🛒 Purchase Products\n"
+        "💰 Check Balance\n"
+        "➕ Add Balance"
+    )
+
+print("Bot Started...")
+bot.infinity_polling()
